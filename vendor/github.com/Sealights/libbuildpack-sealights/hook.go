33d5b497c1ca99cc46f30cb1be850f635f697110
Eugene Titarchuk <eugene.titarchuk@smartexe.com>
2022-05-03 00:19:56 +0300

    install dotnet

--- vendor/github.com/Sealights/libbuildpack-sealights/hook.go
+++ vendor/github.com/Sealights/libbuildpack-sealights/hook.go
@@ -15,7 +15,7 @@ type Command interface {
 	Execute(string, io.Writer, io.Writer, string, ...string) error
 }
 
-// SealightsHook implements libbuildpack.Hook. It downloads and install the Dynatrace OneAgent.
+// SealightsHook implements libbuildpack.Hook. It downloads and install the Sealights OneAgent.
 type SealightsHook struct {
 	libbuildpack.DefaultHook
 	Log     *libbuildpack.Logger
@@ -30,25 +30,7 @@ func NewHook() libbuildpack.Hook {
 	}
 }
 
-func (h *SealightsHook) BeforeCompile(stager *libbuildpack.Stager) error {
-	buildpackDir, err := libbuildpack.GetBuildpackDir()
-	if err != nil {
-		h.Log.Error("Unable to determine buildpack directory: %s", err.Error())
-		os.Exit(9)
-	}
-
-	manifest, err := libbuildpack.NewManifest(buildpackDir, h.Log, time.Now())
-	if err != nil {
-		h.Log.Error("Unable to load buildpack manifest: %s", err.Error())
-		os.Exit(10)
-	}
-
-	h.Log.Info("%v", manifest.ManifestEntries)
-
-	return nil
-}
-
-// AfterCompile downloads and installs the Sealighs agent.
+// AfterCompile downloads and installs the Sealights agent, and modify application start command
 func (h *SealightsHook) AfterCompile(stager *libbuildpack.Stager) error {
 
 	h.Log.Debug("Sealights. Check servicec status...")
@@ -67,20 +49,34 @@ func (h *SealightsHook) AfterCompile(stager *libbuildpack.Stager) error {
 	if err != nil {
 		return err
 	}
+	h.Log.Info("Sealights. Agent installed")
 
 	launcher := NewLauncher(h.Log, conf.Value, installationPath)
 	launcher.ModifyStartParameters(stager)
 
-	// Get buildpack version and language
+	h.Log.Info("Sealights. Service is set up")
 
-	lang := stager.BuildpackLanguage()
-	ver, err := stager.BuildpackVersion()
+	buildpackDir, err := libbuildpack.GetBuildpackDir()
 	if err != nil {
-		h.Log.Warning("Failed to get buildpack version: %v", err)
-		ver = "unknown"
+		h.Log.Error("Unable to determine buildpack directory: %s", err.Error())
+		os.Exit(9)
 	}
 
-	h.Log.Info("Sealights. Language: %s. Buildpack version: %s.", lang, ver)
+	manifest, err := libbuildpack.NewManifest(buildpackDir, h.Log, time.Now())
+	if err != nil {
+		h.Log.Error("Unable to load buildpack manifest: %s", err.Error())
+		os.Exit(10)
+	}
+
+	depinstaller := libbuildpack.NewInstaller(manifest)
+
+	if err = depinstaller.InstallDependency(
+		libbuildpack.Dependency{Name: "dotnet-runtime", Version: "6.0"},
+		filepath.Join(stager.DepDir(), "dotnet-sdk"),
+	); err != nil {
+		h.Log.Info("Sealights. failed to install dotnet")
+		return err
+	}
 
 	return nil
 }
