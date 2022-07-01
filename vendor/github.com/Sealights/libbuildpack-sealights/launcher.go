f04886ad7d984c7829dbea456d13ab3b71ad1ae3
Eugene Titarchuk <etitarchuk@gmail.com>
2022-07-01 15:44:44 +0300

    test

--- vendor/github.com/Sealights/libbuildpack-sealights/launcher.go
+++ vendor/github.com/Sealights/libbuildpack-sealights/launcher.go
@@ -163,6 +163,7 @@ func (la *Launcher) addProfilerConfiguration(agentPath string, collectorId strin
 	la.Log.Error("TEST3")
 	file, err := os.OpenFile(agentEnvFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
+		la.Log.Error(fmt.Sprint(err))
 		return "", err
 	}
 	defer file.Close()
